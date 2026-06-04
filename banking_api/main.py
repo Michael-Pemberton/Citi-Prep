2e7a81dbae1780d7af892ca0d0717cba3c7a4f92
Michael Pemberton <Michael.v.Pemberton@gmail.com>
2026-06-04 13:42:53 -0400

    update

--- banking_api/main.py
+++ banking_api/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Restrict to your Vercel frontend URL in production
+    allow_origins=["https://citi-prep-deployment.vercel.app"],  # Restrict to your Vercel frontend URL in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
